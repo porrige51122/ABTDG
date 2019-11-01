423d8490013dbbc06c8ff92a1ca94dda1e6b77c7
porrige51122 <aidan_smithers@btinternet.com>
2019-11-01 16:50:38 +0000

    Added output folder

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     ],
   },
   output: {
-    path: __dirname + '/dist/',
+    path: __dirname + '/docs/',
     filename: 'js/app.bundle.js',
     chunkFilename: 'js/[name].bundle.js',
   },
